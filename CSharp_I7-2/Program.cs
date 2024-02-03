139ba2a10e37bef2e2f0990ff9619b3e3e4aa9f9
Stanislav Shustov <doctorst.s@gmail.com>
2024-02-03 18:59:28 +0100

    Resolved

--- CSharp_I7-2/Program.cs
+++ CSharp_I7-2/Program.cs
@@ -1,8 +1,8 @@
 ﻿/*Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии.
 Даны два неотрицательных числа m и n.*/
 
-int n = 4;
-int m = 1;
+int n = 3;
+int m = 11;
 
 /*  f(a,b,0) = a+b
     f(a,0,n+1) = A(a,n)
@@ -12,44 +12,39 @@ int m = 1;
     A(n+1,0) = A(n,1)
     A(n+1,m+1) = A(n,A(n+1,m))  */
 
-// int Ackermann0(int n, int m) // WRONG ALGORYTHM
-// {
-//     while (n != 0)
-//     {
-//         if (m == 0)
-//         {
-//             Console.WriteLine($"D - {n}, {m}");
-//             m = 1;
-//             Console.WriteLine($"B - {n}, {m}");
-//         }
-//         else
-//         {
-//             m = Ackermann0(n, m-1);
-//             Console.WriteLine($"A - {n}, {m}");
-//         }
-//         n--;
-//         Console.WriteLine($"C - {n}, {m}");
-//         return m+1;
-//     }
-//     return m+1;
-// }
-
-int Ackermann(int m, int n)
+int Ackermann(int n, int m)
+{
+    while (n != 0)
     {
         if (m == 0)
         {
-            return n + 1;
-        }
-        else if (m > 0 && n == 0)
-        {
-            return Ackermann(m - 1, 1);
-        }
-        else if (m > 0 && n > 0)
-        {
-            return Ackermann(m - 1, Ackermann(m, n - 1));
+            m = 1;
         }
         else
-            return 0;
+        {
+            m = Ackermann(n, m-1);
+        }
+        n--;
     }
+    return m+1;
+}
+
+// int Ackermann(int m, int n)
+//     {
+//         if (m == 0)
+//         {
+//             return n + 1;
+//         }
+//         else if (m > 0 && n == 0)
+//         {
+//             return Ackermann(m - 1, 1);
+//         }
+//         else if (m > 0 && n > 0)
+//         {
+//             return Ackermann(m - 1, Ackermann(m, n - 1));
+//         }
+//         else
+//             return 0;
+//     }
 
 Console.WriteLine($"Akkermann({n},{m}) = {Ackermann(n,m)}");
